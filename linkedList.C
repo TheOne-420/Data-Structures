9f6ba6e099765f436af0101efc7cac208ba583d5
Your Name <tabishsaiyedsahab@gmail.com>
2024-08-20 19:56:57 +0530

    Linked List completed

--- linkedList.C
+++ linkedList.C
@@ -4,36 +4,111 @@
 struct Node {
   int id;
   struct Node *next;
-} * head;
+} * head=NULL;
 
-void insertNode() {
+
+void insertNodeAtEnd() 
+{
   int value;
   struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+  struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
 
   printf("Enter value: ");
   scanf("%d", &value);
 
+  newNode->id = value;
+  newNode->next = NULL;
+  
+
+  if (head == NULL)
+  {
+    
+    head = newNode;
+    printf("Node inserted\n");
+    return;
+  }
+  
+   
+  temp=head;
+  //Reach end of list
+  while (temp->next != NULL) 
+  {
+    temp = temp->next ;
+  }
+  //Assign at end
+  temp->next = newNode;
+  printf("Node inserted\n");
+ 
+}
+void insertNodeAtStart() {
+  int value;
+  struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+
+    printf("Enter value: ");
+    scanf("%d", &value);
+  if (head == NULL)
+  {
+   
+    head->id = value;
+    head->next=NULL;
+    printf("Head inserted\n");
+    return;
+  }
+ 
+
   temp->id = value;
   temp->next = head;
   head = temp;
-
-  
 }
 void display() 
 {
   // Check if list is empty
   if (head == NULL)
-    return;
+    {
+      printf("List is empty\n");
+      return;
+    }
 
   struct Node *temp = head;
 
+  printf("Displaying list\n");
   while (temp != NULL) 
   {
-    printf("Value %d\n", temp->id);
+    printf(" %d->", temp->id);
     temp = temp->next;
   }
 }
-void deleteNode() {
+void deleteNodeAtStart() 
+{
+  if (head == NULL)
+  {
+     printf("List is empty\n");
+     return;
+  }
+  
+  struct Node *temp = head;
+  //Assign next node to head
+  head = temp->next;
+
+//Free 1st node
+  free(temp);
+}
+void deleteNodeAtEnd() 
+{
+  if (head == NULL)
+  {
+    printf("List is empty\n");
+    return;
+
+  }
+  if (head->next == NULL)
+  {
+    free(head);
+    head = NULL;
+    return;
+  }
+  
+  
   struct Node *temp = head;
   struct Node *current = temp;
   while (temp->next != NULL) 
@@ -41,35 +116,47 @@ void deleteNode() {
     current = temp;
     temp = temp->next;
   }
+  current->next = NULL;
+  free(temp);
 }
 int main() {
   int choice;
-  head = (struct Node *)malloc(sizeof(struct Node));
-  head->id = 12;
-  head->next = NULL;
 
   do 
   {
+    printf("\n--------------------------------\n");
     printf("1. Insert node\n");
     printf("2. Display\n");
-    printf("3. Delete\n");
+    printf("3. Delete from start\n");
+    printf("4. Delete from end\n");
+    printf("5. Exit\n");
 
     printf("Enter your choice: ");
     scanf("%d", &choice);
-
+     printf("\n--------------------------------\n");
     switch (choice) {
         case 1:
-            insertNode();
+            insertNodeAtEnd();
             break;
         case 2:
             display();
             break;
         case 3:
-            deleteNode();
+            deleteNodeAtStart();
+            break;
+        case 4:
+            deleteNodeAtEnd();
+            break;
+        case 5:
+           printf("Quitting\n");
             break;
         default:
+              printf("Not a valid option\n");
             break;
     }
-  } while (choice != 4);
+   
+
+  } while (choice != 5);
+  free(head);
   return 0;
 }
